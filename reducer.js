fe1b6c63b349751f40ca69b951440dab8dba4c48
max-powell <github email address>
2019-06-07 14:13:44 +0100

    Done.

--- reducer.js
+++ reducer.js
@@ -1,1 +1,28 @@
-// add code snippets from README
+let state
+const button = document.getElementById('button')
+
+const reducer = (state = {count: 0}, action) => {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+
+}
+
+const render = () => {
+  let container = document.getElementById('container')
+  container.textContent = state.count
+}
+
+const dispatch = action => {
+  state = reducer(state,action)
+  render()
+}
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+dispatch({type: '@@INIT'})
